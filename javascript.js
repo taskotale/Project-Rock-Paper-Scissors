eafde2412b0f01aa6f6947bfc96c461c816eb126
taskotale <110062804+taskotale@users.noreply.github.com>
2022-09-12 04:15:00 -0400

    added loop, adjusted when to call function,add functions to create the selections

--- javascript.js
+++ javascript.js
@@ -1,43 +1,48 @@
-let playerSelection = prompt('Type your choice', 'Rock, Paper or Scissors').toLowerCase();
-
-// making a viable choice
-
-function checkPlayerSelection (signChoice) {
-    if (signChoice !== 'rock' && signChoice !== 'paper' && signChoice !== 'scissors') {
-        signChoice = prompt('not supported, please choose:', 'Rock, Paper or Scissors').toLowerCase();
-    checkPlayerSelection(signChoice); 
-    }
-    playerSelection=signChoice;
+// random selection for computer 
+function getComputerSelection(){
+    const possibleChoices = ['rock','paper','scissors'];
+    const randomChoice = possibleChoices[Math.floor(Math.random()*possibleChoices.length)];
+    return randomChoice;
 }
 
-checkPlayerSelection(playerSelection);
 
-// random selection for computer 
 
-const possibleChoices = ['rock','paper','scissors'];
-const computerSelection = possibleChoices[Math.floor(Math.random()*possibleChoices.length)];
+// making a viable choice, checking if can be used
 
-console.log(playerSelection, computerSelection);
+function getPlayerSelection () {
+    let playerChoice = prompt('Type your choice', 'Rock, Paper or Scissors').toLowerCase();
+    return playerChoice
+}
 
-//comparing selections, finding the winner
+function checkPlayerSelection (validChoice) {
+    if (validChoice !== 'rock' && validChoice !== 'paper' && validChoice !== 'scissors') {
+        validChoice = prompt('not supported, please choose:', 'Rock, Paper or Scissors').toLowerCase();
+        checkPlayerSelection(validChoice); 
+    };
+    playerChoice=validChoice;
+    return playerChoice
+}
+
+
+
+//comparing selections, finding the round winner
 
 
 function getRoundWinner (playerInput,computerInput){
     if (playerInput===computerInput) {
-        console.log('it\'s a tie')
+        console.log('it\'s a tie');
     } else if (playerInput==='rock' && computerInput==='scissors'|| 
                 playerInput==='paper' && computerInput==='rock'||
                 playerInput==='scissors' && computerInput==='paper') {
-        console.log('player wins')
+        console.log(`player wins, ${playerInput} beats ${computerInput}`)
     } else {
-        console.log('comp wins');
+        console.log(`comp wins, ${computerInput} beats ${playerInput}`);
     }
 }    
-getRoundWinner(playerSelection,computerSelection);
-        
 
-
-
-
-
+    for (let i = 0; i<5; i++){
+        let computerSelection = getComputerSelection();
+        let playerSelection = checkPlayerSelection(getPlayerSelection());
+        getRoundWinner(playerSelection,computerSelection);
+    }
 

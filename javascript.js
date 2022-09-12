9b7fd54f220f04a852013e32b54c765398b05eb8
taskotale <110062804+taskotale@users.noreply.github.com>
2022-09-12 04:24:48 -0400

    formatted code

--- javascript.js
+++ javascript.js
@@ -1,48 +1,49 @@
 // random selection for computer 
 
-function getComputerSelection(){
-    const possibleChoices = ['rock','paper','scissors'];
-    const randomChoice = possibleChoices[Math.floor(Math.random()*possibleChoices.length)];
+function getComputerSelection() {
+    const possibleChoices = ['rock', 'paper', 'scissors'];
+    const randomChoice = possibleChoices[Math.floor(Math.random() * possibleChoices.length)];
     return randomChoice;
 }
 
 // making a viable choice for user/player
 
-function getPlayerSelection () {
+function getPlayerSelection() {
     let playerChoice = prompt('Type your choice', 'Rock, Paper or Scissors').toLowerCase();
     return playerChoice
 }
 
 // checking if selection is valid
 
-function checkPlayerSelection (validChoice) {
+function checkPlayerSelection(validChoice) {
     if (validChoice !== 'rock' && validChoice !== 'paper' && validChoice !== 'scissors') {
         validChoice = prompt('not supported, please choose:', 'Rock, Paper or Scissors').toLowerCase();
-        checkPlayerSelection(validChoice); 
+        checkPlayerSelection(validChoice);
     };
-    playerChoice=validChoice;
+    playerChoice = validChoice;
     return playerChoice
 }
 
 //comparing selections, finding the round winner per round
 
-function getRoundWinner (playerInput,computerInput){
-    if (playerInput===computerInput) {
+function getRoundWinner(playerInput, computerInput) {
+    if (playerInput === computerInput) {
         console.log('it\'s a tie');
-    } else if (playerInput==='rock' && computerInput==='scissors'|| 
-                playerInput==='paper' && computerInput==='rock'||
-                playerInput==='scissors' && computerInput==='paper') {
+    } else if (playerInput === 'rock' && computerInput === 'scissors' ||
+        playerInput === 'paper' && computerInput === 'rock' ||
+        playerInput === 'scissors' && computerInput === 'paper') {
         console.log(`player wins, ${playerInput} beats ${computerInput}`)
     } else {
         console.log(`comp wins, ${computerInput} beats ${playerInput}`);
     }
-}    
+}
 
 // a game made of 5 rounds
 
-    for (let i = 0; i<5; i++){
-        let computerSelection = getComputerSelection();
-        let playerSelection = checkPlayerSelection(getPlayerSelection());
-        getRoundWinner(playerSelection,computerSelection);
-    }
+for (let i = 0; i < 5; i++) {
+    let computerSelection = getComputerSelection();
+    let playerSelection = checkPlayerSelection(getPlayerSelection());
+    getRoundWinner(playerSelection, computerSelection);
+    console.log(playerSelection, computerSelection);
+}
 
